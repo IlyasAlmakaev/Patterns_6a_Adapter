9ad50cbac059438226e70401a520655e8d99bc08
Ильяс <i.almakaev@gmail.com>
2017-07-13 15:33:43 +0700

    Initial Commit

---
+++ Patterns_6a_Adapter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Patterns_6a_Adapter
+//
+//  Created by Ильяс on 13.07.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
